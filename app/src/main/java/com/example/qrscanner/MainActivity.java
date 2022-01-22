c915937c26c6919e2f2ddec029eb56b0c489ba90
fjonass <jsabini@pdx.edu>
2022-01-21 22:26:46 -0800

    intropage
    added intropage
    progress work on settings

--- app/src/main/java/com/example/qrscanner/MainActivity.java
+++ app/src/main/java/com/example/qrscanner/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,11 +21,15 @@ public class MainActivity extends AppCompatActivity {
 
     public static String activeEmail = null;
     public String name = null;
+    private int intro = 0;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        StartActivity(Intropage.class);
+
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Cov-19 Tracker");
 
